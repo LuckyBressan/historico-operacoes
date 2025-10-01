76ac955d6b414ca50040bf6b63a88a48800eccfb
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2025-10-01 20:59:45 -0300

    feat: change url API;

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://127.0.0.1:${import.meta.env.VITE_PORT_BACKEND}/`,
-
+    baseURL: `http://localhost:${import.meta.env.VITE_PORT_BACKEND}/`,
 })
 export default api;
